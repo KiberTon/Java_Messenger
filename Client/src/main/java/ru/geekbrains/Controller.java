282702543e26b79f23a0b9631c16f2f12f7df030
Anton Kibekin <kibekin1984anton@gmail.com>
2020-10-09 21:44:06 +0300

    Замена Scaner на DataPrint

---
+++ Client/src/main/java/ru/geekbrains/Controller.java
@@ -0,0 +1,11 @@
+package ru.geekbrains;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Controller {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+}
