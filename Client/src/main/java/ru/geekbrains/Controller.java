32263afe3561cfb844c2b1c4c2a684a8945be046
Anton Kibekin <kibekin1984anton@gmail.com>
2020-10-09 21:49:24 +0300

    Создание socket

--- Client/src/main/java/ru/geekbrains/Controller.java
+++ Client/src/main/java/ru/geekbrains/Controller.java
@@ -1,11 +1,24 @@
 package ru.geekbrains;
 
+import javafx.fxml.Initializable;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            socket = new Socket("localhost", 8189);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
